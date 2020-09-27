80c520b38ec4281dde3e99c8f75a06c34cea8224
Santiago González <37671738+sangonz193@users.noreply.github.com>
2020-09-26 22:00:10 -0300

    move database config to another file

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,8 +1,7 @@
 import * as path from "path";
-import { ConnectionOptions } from "typeorm";
 import urlJoin from "url-join";
 
-import { entities } from "./entities";
+import { dbConnectionOptions } from "./config/dbConnectionOptions";
 
 const { env } = process;
 const {
@@ -12,10 +11,6 @@ const {
 	ACCESS_TOKEN_DURATION,
 	REFRESH_TOKEN_SECRET,
 	REFRESH_TOKEN_DURATION,
-	WRITE_DB_HOST,
-	WRITE_DB_NAME,
-	WRITE_DB_USERNAME,
-	WRITE_DB_PASSWORD,
 } = env;
 
 if (!env.FILES_PATH) throw new Error("FILES_PATH not defined");
@@ -36,39 +31,15 @@ if (!env.PORT) throw new Error("PORT variable not defined");
 const PORT = Number(env.PORT);
 if (!PORT) throw new Error("Invalid PORT value. Must be number");
 
-if (!WRITE_DB_HOST) throw new Error("WRITE_DB_HOST variable not defined");
-
 if (!env.WRITE_DB_PORT) throw new Error("WRITE_DB_PORT variable not defined");
 const WRITE_DB_PORT = Number(env.WRITE_DB_PORT);
 if (!WRITE_DB_PORT) throw new Error("Invalid WRITE_DB_PORT value. Must be number");
-
-if (!WRITE_DB_NAME) throw new Error("WRITE_DB_NAME variable not defined");
-if (!WRITE_DB_USERNAME) throw new Error("WRITE_DB_USERNAME variable not defined");
-if (!WRITE_DB_PASSWORD) throw new Error("WRITE_DB_PASSWORD variable not defined");
 
 const ASSETS_PATH = path.join(FILES_PATH, "assets");
 const ASSETS_URL = urlJoin(FILES_URL, "assets");
 
 const COURSE_ICONS_PATH = path.join(ASSETS_PATH, "course-icons");
 const COURSE_ICONS_URL = urlJoin(ASSETS_URL, "course-icons");
-
-const isProduction = process.env.NODE_ENV === "production";
-
-const dbConnectionOptions: ConnectionOptions = {
-	type: "postgres",
-	database: WRITE_DB_NAME,
-	host: WRITE_DB_HOST,
-	port: WRITE_DB_PORT,
-	username: WRITE_DB_USERNAME,
-	password: WRITE_DB_PASSWORD,
-	schema: "openfing",
-	entities: Object.values(entities),
-	logging: !isProduction,
-	migrations: [isProduction ? "dist/migrations/*.js" : "src/migrations/*.ts"],
-	cli: {
-		migrationsDir: "src/migrations",
-	},
-};
 
 export const appConfig = {
 	dbConnectionOptions,
