ddd51f24dcdf990118d41da7438319b86d684102
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-01-24 13:25:57 -0300

    sort latest classes by publishedAt

--- src/resolvers/CourseClassList/courseEdition.resolver.ts
+++ src/resolvers/CourseClassList/courseEdition.resolver.ts
@@ -2,7 +2,7 @@ import { Resolvers } from "../../generated/graphql.types";
 import { getCourseEditionParent } from "../CourseEdition/CourseEdition.parent";
 
 const resolver: Resolvers["CourseClassList"]["courseEdition"] = async (parent, _, { dataLoaders, includeHidden }) => {
-	if (typeof parent.course_edition_id !== "number") return null;
+	if (parent.course_edition_id === null) return null;
 
 	const courseEdition = await dataLoaders.courseEdition.load({
 		id: parent.course_edition_id,
