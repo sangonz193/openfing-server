ae368f079c51dd788fcd72609f232911b89a2064
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-06-06 16:11:10 -0300

    add error handling for generateSqlTypes

--- cli/commands/generate-files/generateSqlTypes.ts
+++ cli/commands/generate-files/generateSqlTypes.ts
@@ -1,4 +1,5 @@
 import { fs } from "@sangonz193/utils/node/fs"
+import chalk from "chalk"
 import path from "path"
 import { spawn } from "promisify-child-process"
 import * as zg from "zapatos/generate"
@@ -9,25 +10,30 @@ import { databaseConfig } from "../../../src/database/database.config"
 export async function generateSqlTypes() {
 	const zapatosOutDirOption = path.resolve(projectPath, "src", "database")
 	const outExt = ".generated.d.ts"
-	await zg.generate({
-		db: {
-			database: databaseConfig.typeormConfig.database,
-			host: databaseConfig.typeormConfig.host,
-			user: databaseConfig.typeormConfig.username,
-			password: databaseConfig.typeormConfig.password,
-			port: databaseConfig.typeormConfig.port,
-			ssl: undefined,
-		},
-		outDir: zapatosOutDirOption,
-		outExt: outExt,
-		schemas: {
-			openfing: {
-				include: "*",
-				exclude: [],
+	try {
+		await zg.generate({
+			db: {
+				database: databaseConfig.typeormConfig.database,
+				host: databaseConfig.typeormConfig.host,
+				user: databaseConfig.typeormConfig.username,
+				password: databaseConfig.typeormConfig.password,
+				port: databaseConfig.typeormConfig.port,
+				ssl: undefined,
 			},
-		},
-		schemaJSDoc: true,
-	})
+			outDir: zapatosOutDirOption,
+			outExt: outExt,
+			schemas: {
+				openfing: {
+					include: "*",
+					exclude: [],
+				},
+			},
+			schemaJSDoc: true,
+		})
+	} catch (error: unknown) {
+		console.log(chalk.red(`Could not generate SQL types.`), error)
+		return
+	}
 
 	const generatedEslintIgnoreFilePath = path.resolve(zapatosOutDirOption, "zapatos", ".eslintrc.json")
 	await fs.unlink(generatedEslintIgnoreFilePath)
