266b63f9095c79c8e67d1974988f2339f150baee
Santiago González <37671738+sangonz193@users.noreply.github.com>
2021-06-12 21:00:32 -0300

    minor fix

--- src/api/graphql/middlewares/withAuthenticatedUser.ts
+++ src/api/graphql/middlewares/withAuthenticatedUser.ts
@@ -8,7 +8,7 @@ import {
 } from "../AuthenticationError/AuthenticationError.parent"
 import { ResolverFn } from "../schemas.types"
 
-export function withUserFromSecret<TReturn, TParent, TContext extends RequestContext, TArgs>(
+export function withAuthenticatedUser<TReturn, TParent, TContext extends RequestContext, TArgs>(
 	resolver: ResolverFn<TReturn, TParent, SafeOmit<TContext, "user"> & Required<Pick<RequestContext, "user">>, TArgs>
 ): ResolverFn<TReturn | AuthenticationErrorParent, TParent, TContext, TArgs> {
 	return async (parent, args, context, info) => {
